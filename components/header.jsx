273f04eee5afd187d5633d07c1204dc705af6129
ZoxidovSunnatilla <sunnatzoxidov3@gmail.com>
2024-01-26 14:59:11 +0500

    Sunnat commit

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const header = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+    const [isOpen] = useState(false)
     return (
         <div className='  '>
             <div className=" bg-costum-gray hidden md:flex">
@@ -40,7 +42,7 @@ const header = () => {
                         <div className='flex relative gap-2 items-center'>
                             <div className='relative'>
                                 <img src="/images/envelope.svg" alt="" />
-                                <div className="bg-costum-orange w-auto absolute rounded-full w-4 h-4 justify-center flex items-center -top-2 -right-2 text-xs text-white text-costum-text-black font-normal">12</div>
+                                <div className="bg-costum-orange w-auto absolute rounded-full w-5 h-5 justify-center flex items-center -top-3 -right-2.5 text-xs text-white text-costum-text-black font-normal">12</div>
 
                             </div>
                             <a href='' className=' text-sm text-costum-text-black font-normal '>Message</a>
@@ -100,6 +102,180 @@ const header = () => {
                             <p className="text-costum--text-black">ELECTRONICS</p>
                             <p className="text-costum--text-black">OFFICE EQUIPMENT</p>
 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="menu" className={`min-h-7 py-12 z-50  w-full bg-white hidden`}>
+                <div className="container mx-auto flex justify-between">
+                    <div className="w-auto">
+                        <div className="flex gap-2 items-center mb-2">
+                            <img src="/images/menu_img.svg" alt="" />
+                            <p className="text-base text-costum-blue font-medium">Kitchen</p>
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen, Dining & Bar
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Small kitchen Appliances
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Tools and Gadgets
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Cookware
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Bakeware
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Storage
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Flatware
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Knives and cutlery
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Bar accessories
+                            </a>
+                        </div>
+                        <div className=" w-auto  flex item-center gap-1 h-6 ">
+                            <p className="text-costum-blue text-base font-medium ">Views all markets</p>
+                            <img src="/images/arrow_forward.svg" alt="" />
+                        </div>
+                    </div>
+                    <div className="w-auto">
+                        <div className="flex gap-2 items-center mb-2">
+                            <img src="/images/menu_img.svg" alt="" />
+                            <p className="text-base text-costum-blue font-medium">Kitchen</p>
+                        </div>
+                        <div className="flex flex-col mb-10">
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen, Dining & Bar
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Small kitchen Appliances
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Tools and Gadgets
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Cookware
+                            </a>
+                           
+                        </div>
+
+                        <div className="flex gap-2 items-center mb-2">
+                            <img src="/images/menu_img.svg" alt="" />
+                            <p className="text-base text-costum-blue font-medium">Kitchen</p>
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen, Dining & Bar
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Small kitchen Appliances
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Tools and Gadgets
+                            </a>
+                           
+                           
+                        </div>
+                    </div>
+                    <div className="w-auto">
+                        <div className="flex gap-2 items-center mb-2">
+                            <img src="/images/menu_img.svg" alt="" />
+                            <p className="text-base text-costum-blue font-medium">Kitchen</p>
+                        </div>
+                        <div className="flex flex-col mb-10">
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen, Dining & Bar
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Small kitchen Appliances
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Tools and Gadgets
+                            </a>
+                       
+                           
+                        </div>
+
+                        <div className="flex gap-2 items-center mb-2">
+                            <img src="/images/menu_img.svg" alt="" />
+                            <p className="text-base text-costum-blue font-medium">Kitchen</p>
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen, Dining & Bar
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Small kitchen Appliances
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Kitchen Tools and Gadgets
+                            </a>
+                            <a href="" className="text-base text-costum-text-black mb-3">
+                                Cookware
+                            </a>
+                           
+                           
+                        </div>
+                    </div>
+                    <div className="w-1/4 pr-10 h-96 scroll-smoth overflow-y-auto">
+                        <div className="flex flex-wrap justify-between mb-10 gap-3">
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                            <div className="flex justify-center items-center h-14 w-24 rounded-lg bg-star">
+                                <p className="text-base font-medium text-costum-text-black">LOGO</p>
+                            </div>
+                        </div>
+                        <div className="cart mb-3">
+                            <div className="flex px-6 py-4 border border-solid gap-2 border-costum-blue rounded-3xl">
+                                <img src="/images/menu_product_img.svg" alt="" />
+                                <p className="text-base text-costum-text-black">ELECTRICAL Grill Mastergrill <br /> SUP412 <span className='text-costum-orange text-sm'>-30%</span></p>
+                            </div>
+                        </div>
+                        <div className="cart mb-3">
+                            <div className="flex px-6 py-4 border border-solid gap-2 border-costum-blue rounded-3xl">
+                                <img src="/images/menu_product_img.svg" alt="" />
+                                <p className="text-base text-costum-text-black">ELECTRICAL Grill Mastergrill <br /> SUP412 <span className='text-costum-orange text-sm'>-30%</span></p>
+                            </div>
+                        </div>
+                        <div className="cart mb-3">
+                            <div className="flex px-6 py-4 border border-solid gap-2 border-costum-blue rounded-3xl">
+                                <img src="/images/menu_product_img.svg" alt="" />
+                                <p className="text-base text-costum-text-black">ELECTRICAL Grill Mastergrill <br /> SUP412 <span className='text-costum-orange text-sm'>-30%</span></p>
+                            </div>
+                        </div>
+                        <div className="cart mb-3">
+                            <div className="flex px-6 py-4 border border-solid gap-2 border-costum-blue rounded-3xl">
+                                <img src="/images/menu_product_img.svg" alt="" />
+                                <p className="text-base text-costum-text-black">ELECTRICAL Grill Mastergrill <br /> SUP412 <span className='text-costum-orange text-sm'>-30%</span></p>
+                            </div>
+                        </div>
+                        <div className="cart mb-3">
+                            <div className="flex px-6 py-4 border border-solid gap-2 border-costum-blue rounded-3xl">
+                                <img src="/images/menu_product_img.svg" alt="" />
+                                <p className="text-base text-costum-text-black">ELECTRICAL Grill Mastergrill <br /> SUP412 <span className='text-costum-orange text-sm'>-30%</span></p>
+                            </div>
                         </div>
                     </div>
                 </div>
